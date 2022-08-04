45fa5f137c9873fb19c0385bb2c065d7d4458f45
Ruanruanx <zxin7@uwo.ca>
2022-08-04 20:34:43 +0000

    mealActivity

--- survey.js
+++ survey.js
@@ -1,20 +1,28 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question("What's your name? Nicknames are also acceptable :)",(answer)=>{
+rl.question("What's your name? Nicknames are also acceptable :)", (answer) => {
   let name = answer;
-  rl.question("What's an activity you like doing?",(answer)=>{
-  let hobby = answer;
-    rl.question("What do you listen to while doing that?",(answer)=>{
+  rl.question("What's an activity you like doing?", (answer) => {
+    let hobby = answer;
+    rl.question("What do you listen to while doing that?", (answer) => {
       let music = answer;
-      console.log(name+hobby+music);
-      rl.close();
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
+        let meal = answer;
+        rl.question("What's your favourite thing to eat for that meal?", (answer) => {
+          let mealActivity = answer;
+          console.log(`My name is ${name}. I like ${hobby} and I always listen to ${music} when I ${hobby}. My favourite meal in a day is ${meal}, I enjoy doing ${mealActivity} while having ${meal}`);
+          rl.close();
+        })
+
+      })
+
     })
- 
+
   })
 
 })
